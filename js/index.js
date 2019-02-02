eff471b03e638bb31e759a9b3907a55a4f9979eb
Christian-Fjord <fjord.christian@gmail.com>
2019-02-02 17:57:35 -0600

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 // Your code goes here
+let myAnchors = document.querySelector('.nav-link');
+
+myAnchors.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'red';
+});
+
+myAnchors.addEventListener('mouseleave', (event) => {
+  event.target.style.color = 'black';
+});
+
+
+let myFunBus = document.querySelector('.logo-heading');
+
+myFunBus.addEventListener('click', 
+(event) => {
+  alert('Welcome to the FUN BUS');
+});
+
+let myPic = document.querySelector('.bus-img');
+myPic.addEventListener('dblclick', 
+(event) => {
+  event.target.style.border = "3px solid black";
+
+})
+
+let allThings = document.querySelector('body');
+
+allThings.addEventListener('keydown', 
+(event) => {
+  event.target.style.backgroundColor = "grey";
+})
+
+
+allThings.addEventListener('keyup', 
+(event) => {
+  event.target.style.backgroundColor = "white";
+});
+
+
+let imgContent = document.querySelector('.img-content');
+
+allThings.addEventListener('mousemove', 
+(event) => {
+  event.target.style.border = "4px solid orange";
+  event.stopPropagation();
+});
+
+
+
+
+
+
+
